e305e1dd296c337b8bb48021e41b1d290a9a822c
khongchai <world1955@hotmail.com>
2022-10-01 16:33:19 +0700

    PageDLLData now behaves more like an actual DLL data.

--- lib/dynamic_routes/page_dll_data.dart
+++ lib/dynamic_routes/page_dll_data.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
 
-/// The doubly-linked-list-kind-of representation that is used to help ensure
-/// that the next page that is pushed is the correct one.
-class PageDLLData {
-  final Widget? previousPage;
-  final Widget currentPage;
-  final Widget? nextPage;
-  final int index;
+enum PageDLLTraversalDirection {
+  left,
+  right,
+}
 
-  const PageDLLData({
-    required this.previousPage,
-    required this.currentPage,
-    required this.nextPage,
-    required this.index,
+/// A not-really-doubly-linked-list-kind-of representation that is used to help
+/// ensure that the next page that is pushed is the correct one.
+class PageDLLData {
+  PageDLLData? previousPage;
+
+  /// The page tied to this DLL.
+  final Widget widget;
+  PageDLLData? nextPage;
+
+  PageDLLData({
+    this.previousPage,
+    required this.widget,
+    this.nextPage,
   });
 
-  bool isFirstPage() {
-    final isFirstPage = previousPage == null;
-    if (isFirstPage) {
-      assert(index == 0);
-    }
+  int getTraversalSteps(PageDLLTraversalDirection direction,
+      [PageDLLData? pageDLLData, i = 0]) {
+    final page = pageDLLData ?? this;
 
-    return isFirstPage;
+    if (direction == PageDLLTraversalDirection.left) {
+      if (page.isFirstPage()) return i;
+      return getTraversalSteps(direction, page.previousPage, i + 1);
+    } else {
+      if (page.isLastPage()) return i;
+      return getTraversalSteps(direction, page.nextPage, i + 1);
+    }
   }
+
+  void setAsNext(PageDLLData nextPage) {
+    this.nextPage = nextPage;
+    nextPage.previousPage = this;
+  }
+
+  void setAsPrevious(PageDLLData previousPage) {
+    this.previousPage = previousPage;
+    previousPage.nextPage = this;
+  }
+
+  bool isFirstPage() => previousPage == null;
 
   bool isLastPage() => nextPage == null;
 }
