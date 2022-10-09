144ca8bc9a2e2ea406d470837afde38b04056cac
khongchai <world1955@hotmail.com>
2022-10-09 20:32:26 +0700

    added more tests for the cache

--- test/cache_test.dart
+++ test/cache_test.dart
@@ -4,49 +4,73 @@ import 'pages.dart';
 
 import "package:flutter/material.dart";
 
+import 'testing_utils.dart';
+
 void main() {
   group("Cache test", () {
     testWidgets("Test read and write to cache", (WidgetTester tester) async {
       //Assign a value to the routeCache in the first page
       final mockCacheData = {"some-key": 1, "some-other-key": 2};
-      const pushFirstButtonKey = Key("pushFirstButton");
-      const firstPagePushNextKey = Key("firstPagePushNextKey");
-      const secondPagePushNextKey = Key("secondPagePushNextKey");
-      const thirdWidget = ParticipatorWidget();
 
-      await tester.pumpWidget(const InitiatorWidget(
-        pushFirstButtonKey: pushFirstButtonKey,
-        participatorPages: [
-          ParticipatorWidget(pushNextButtonKey: firstPagePushNextKey),
-          ParticipatorWidget(
-            pushNextButtonKey: secondPagePushNextKey,
-          ),
-          thirdWidget
-        ],
-      ));
+      final participators = TestingUtils.generateParticipatorWidget(3);
+      final initiator = TestingUtils.generateInitiatorWidget(participators);
+
+      await tester.pumpWidget(initiator);
+
       final InitiatorWidgetState initiatorState =
           tester.state(find.byType(InitiatorWidget));
 
-      await tester.tap(find.byKey(pushFirstButtonKey));
+      await tester.tap(find.byKey(initiator.pushFirstButtonKey!));
       initiatorState.dynamicRoutesInitiator.setCache(mockCacheData);
       await tester.pumpAndSettle();
 
-      await tester.tap(find.byKey(firstPagePushNextKey));
+      final firstParticipatorState = TestingUtils.getParticipatorStateFromKey(
+          tester, participators[0].key!);
+      await tester.tap(find.byKey(participators[0].pushNextButtonKey!));
       await tester.pumpAndSettle();
 
-      await tester.tap(find.byKey(secondPagePushNextKey));
+      final secondParticipatorState = TestingUtils.getParticipatorStateFromKey(
+          tester, participators[1].key!);
+      await tester.tap(find.byKey(participators[1].pushNextButtonKey!));
       await tester.pumpAndSettle();
 
-      final ParticipatorWidgetState stateToTest =
-          tester.state(find.byWidget(thirdWidget));
+      final thirdParticipatorState = TestingUtils.getParticipatorStateFromKey(
+          tester, participators[2].key!);
+      await tester.tap(find.byKey(participators[2].pushNextButtonKey!));
+      await tester.pumpAndSettle();
 
-      expect(stateToTest.dynamicRoutesParticipator.getCache(), mockCacheData);
+      expect(initiatorState.dynamicRoutesInitiator.getCache(), mockCacheData);
+      expect(firstParticipatorState.dynamicRoutesParticipator.getCache(),
+          mockCacheData);
+      expect(secondParticipatorState.dynamicRoutesParticipator.getCache(),
+          mockCacheData);
+      expect(thirdParticipatorState.dynamicRoutesParticipator.getCache(),
+          mockCacheData);
     });
 
     testWidgets(
-        "Once a navigation scope is disposed, the cache should be cleared",
-        (tester) async {
+        "Once a navigation scope is disposed, the cache should no longer have a "
+        "reference to the value.", (tester) async {
+      const cacheData = 0;
 
-        });
+      final participators = TestingUtils.generateParticipatorWidget(3);
+      final initiator = TestingUtils.generateInitiatorWidget(participators);
+
+      final initiatorState =
+          await TestingUtils.pushInitiatorPageWithFullStateControl(
+              initiator: initiator,
+              participators: participators,
+              tester: tester);
+
+      final dynamicRoutesInitiator = initiatorState.dynamicRoutesInitiator;
+
+      dynamicRoutesInitiator.setCache(cacheData);
+
+      expect(dynamicRoutesInitiator.getCache(), cacheData);
+
+      dynamicRoutesInitiator.dispose();
+
+      expect(dynamicRoutesInitiator.getCache(), null);
+    });
   });
 }
